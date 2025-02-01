eb04d514445878e135699677bb769a13d6bb0a87
Mahmood_Reda <mr4141705@gmail.com>
2025-02-01 17:06:11 +0300

    Adding all files

---
+++ src/C-Coding-Adventures.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : C-Coding-Adventures.c
+ Author      : mahmood_Reda
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	return 0;
+}
